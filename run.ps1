9732332fbcba832a784c7d437827b570cf251e0c
Hunter <hsta@beachmont.xyz>
2025-11-10 12:31:37 -0500

    fix(run.ps1): non-hardcoded path and fix path sep for pros-cli

--- run.ps1
+++ run.ps1
@@ -1,8 +1,8 @@
 # Setup the correct path for running the build system
 
-$env:Path += ";/Users/Student/AppData/Roaming/Code/User/globalStorage/sigbots.pros/install/pros-cli-windows"
-$env:Path += ";C:\Users\Student\AppData\Roaming\Code\User\globalStorage\sigbots.pros\install\pros-toolchain-windows\usr\bin"
-$env:Path += ";C:\Users\Student\AppData\Roaming\Code\User\globalStorage\sigbots.pros\install\vex-vexcom-windows"
+$env:Path += ";$Home\AppData\Roaming\Code\User\globalStorage\sigbots.pros\install\pros-cli-windows"
+$env:Path += ";$Home\AppData\Roaming\Code\User\globalStorage\sigbots.pros\install\pros-toolchain-windows\usr\bin"
+$env:Path += ";$Home\AppData\Roaming\Code\User\globalStorage\sigbots.pros\install\vex-vexcom-windows"
 
 make -j
 pros upload
